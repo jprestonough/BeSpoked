de37c5d05217e0055e35175ae041d0afc61decf3
preston <jprestonough@gmail.com>
2022-08-24 15:23:19 -0400

    Add project files.

---
+++ BeSpoked/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace BeSpoked.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Style { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public int QuantityNbr { get; set; }
+        public decimal CommissionPct { get; set; }
+    }
+}
